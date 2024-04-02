647e7b0132a2091eb6847aff464d3dbfefd0c071
Facundo Ríos <Facundorios2005@gmail.com>
2024-04-02 20:13:13 -0300

    feat: finished exercise 3

--- number_spiral (3).py
+++ number_spiral (3).py
@@ -1,18 +1,23 @@
-def espiral_de_numeros(filas, columnas):
-    #Condicional para saber si el el parametro de filas es mayor que el de columnas
-    if filas > columnas:
-    #En caso de ser verdadero, calculamos el cuadrado del anterior numero de filas, y le sumamos el numero de las columnas.
-        return (filas - 1) ** 2 + columnas
-    #En caso contrario:
-    else:
-        #Verificamos que x sea columnaas sea par.
-        if columnas % 2 == 0:
-        #Devuelva el anterior de columna al cuadrado, sumado el numero filas
-            return (columnas - 1) ** 2 + filas
+def espiral_de_numeros(x, y):
+    # Condicional para saber si el el parametro de filas es mayor que el de columnas
+    if x > y:
+        # En caso de ser verdadero usamos otro condicional, en donde determinamos el filas es par.
+        if x % 2 == 0:
+            return (x**2) - y + 1
+        # En caso contrario:
         else:
-        #Si columnas es impar, se calcula el cuadrado y selo resta por numero de filas - 1
-            return (columnas **  2) - filas + 1
-    
-print(espiral_de_numeros(4,4))
-# assert espiral_de_numeros(2, 2) == 3, "Error en el caso de prueba"
-# print("Todos los casos de prueba han pasado correctamente")
+        #Retorna el anterior de filas alcuadrado, más columnas
+            return (x - 1) ** 2 + y
+    else:
+        # Verificamos que x sea columnaas sea par.
+        if y % 2 == 0:
+            # Devuelva el anterior de columna al cuadrado, sumado el numero filas
+            return (y - 1) ** 2 + x
+        else:
+            # Si columnas es impar, se calcula el cuadrado y selo resta por numero de filas - 1.
+            return (y**2) - x + 1
+
+
+assert espiral_de_numeros(2, 2) == 3, "Error en el caso de prueba"
+print("Todos los casos de prueba han pasado correctamente")
+
