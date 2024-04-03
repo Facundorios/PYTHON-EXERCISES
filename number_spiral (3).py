6462b878fa4862d76bb5824ba4d7b05d805461c8
Facundo Ríos <Facundorios2005@gmail.com>
2024-04-03 00:19:42 -0300

    finished excercises

--- number_spiral (3).py
+++ number_spiral (3).py
@@ -1,23 +1,38 @@
-def espiral_de_numeros(x, y):
-    # Condicional para saber si el el parametro de filas es mayor que el de columnas
-    if x > y:
-        # En caso de ser verdadero usamos otro condicional, en donde determinamos el filas es par.
-        if x % 2 == 0:
-            return (x**2) - y + 1
-        # En caso contrario:
-        else:
-        #Retorna el anterior de filas alcuadrado, más columnas
-            return (x - 1) ** 2 + y
+def espiral_de_numeros(filas, columnas):
+    #Condicional para saber si el el parametro de filas es mayor que el de columnas
+    if filas > columnas:
+    #En caso de ser verdadero usamos
+
+        return (filas - 1) ** 2 + columnas + 1
+    #En caso contrario:
     else:
-        # Verificamos que x sea columnaas sea par.
-        if y % 2 == 0:
-            # Devuelva el anterior de columna al cuadrado, sumado el numero filas
-            return (y - 1) ** 2 + x
+        #Verificamos que x sea columnaas sea par.
+        if columnas % 2 == 0:
+        #Devuelva el anterior de columna al cuadrado, sumado el numero filas
+            return (columnas - 1) ** 2 + filas
         else:
-            # Si columnas es impar, se calcula el cuadrado y selo resta por numero de filas - 1.
-            return (y**2) - x + 1
+        #Si columnas es impar, se calcula el cuadrado y selo resta por numero de filas - 1
+            return (columnas **  2) - filas + 1
+    
+print(espiral_de_numeros(4,4))
+# assert espiral_de_numeros(2, 2) == 3, "Error en el caso de prueba"
+# print("Todos los casos de prueba han pasado correctamente")
+
+#Hace una funcion que toma como parametros x e y, que sirven para ubicar el numero en el espiral.
+def espiral_de_numeros(filas, columnas):
+#
+    if filas > columnas:
+        if filas % 2 == 0:
+            return (filas ** 2) - columnas + 1
+        else:
+            return ( filas - 1 ) ** 2 + columnas
+    else:
+        if columnas % 2 == 0:
+            return ( columnas - 1 ) ** 2 + filas
+        else:
+            return columnas ** 2 - filas + 1
+
 
 
 assert espiral_de_numeros(2, 2) == 3, "Error en el caso de prueba"
 print("Todos los casos de prueba han pasado correctamente")
-
---
+++ palindrome_reorder (4).py
@@ -0,0 +1,35 @@
+def ordenador_palindromo(n):
+    # Verificar si n es una cadena de texto del alfabeto inglés y no supera 10^6
+    if not isinstance(n, str) or len(n) > 10**6:
+        return "n no cumple con las condiciones para verificar si es palindromo, o si puede ser uno."
+    else:
+        contador_letras = {}  # Contar las ocurrencias de cada letra en la cadena
+    # Bucle for in para recorrer n y contar las repeticiones de cada letra
+    for letra in n:
+        contador_letras[letra] = contador_letras.get(letra, 0) + 1
+
+    # creamos una variable para la letra impar
+    letra_impar = ""
+
+    palindromo = ""
+
+    # Recorrer contador_letras para saber si hay mas de una letra impar
+    for letra, count in contador_letras.items():
+        if count % 2 != 0:
+            if letra_impar:
+                # Si hay más de una letra con frecuencia impar, devolver "NO SOLUTION"
+                return "NO SOLUTION"
+            else:  # En caso contrario
+                # Se guarda la letra con frecuencia impar y se construye la mitad del palíndromo
+                letra_impar = letra
+        palindromo += letra * (count // 2)
+    # Se retorna el palíndromo completo
+    return palindromo + letra_impar + palindromo[::-1]
+
+
+# print(ordenador_palindromo("qqwweerrttyyuuiiopp"))
+
+# Caso de prueba
+assert ordenador_palindromo("aabbc") == "abcba", "Error en el casode prueba"
+print("Todos los casos de prueba han pasado correctamente")
+
